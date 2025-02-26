a550e8f10c8a21ea941da1164f119678f859e145
BiraBalaZ <erick.m.anjos@gmail.com>
2025-02-25 23:20:58 -0300

    Adding context

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,30 @@ from django.shortcuts import render
 
 
 def blog(request):
+    print('blog')
+
+    context = {
+        'text': 'Olá Blog',
+        'title': 'Blog',
+    }
+
     return render(
         request,
         'blog/index.html',
+        context,
     )
 
 
 def exemplo(request):
+    print('exemplo')
+
+    contexto = {
+        'text': 'Olá Exemplo',
+        'title': 'Exemplo',
+    }
+
     return render(
         request,
         'blog/exemplo.html',
+        contexto,
     )
--- home/views.py
+++ home/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render
 
 
 def home(request):
+    print('home')
+
+    context = {
+        'text': 'Olá Home',
+        'title': 'Home',
+    }
+
     return render(
         request,
         'home/index.html',
+        context,
     )
